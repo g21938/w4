802664c0f775c9aae3f800870db444a4114bc637
g21938 <113569154+g21938@users.noreply.github.com>
2022-10-02 21:32:28 +0900

    Update main.js

--- assessment-finished/main.js
+++ assessment-finished/main.js
@@ -28,10 +28,10 @@ function result() {
 
   if (customName.value !== '') {
     const name = customName.value;
-    newStory = newStory.replace("bob", name);
+    newStory = newStory.replace('Bob', name);
   }
 
-  if (document.getElementById("UK").checked) {
+  if (document.getElementById("uk").checked) {
     const weight = `${Math.round(300*0.0714286)} stone`;
     const temperature =  `${Math.round((94-32) * 5 / 9)} centigrade`;
     newStory = newStory.replace('94 fahrenheit', temperature);
