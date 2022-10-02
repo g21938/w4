b37f418e589cf8871105f30c9144d89687640c2b
g21938 <113569154+g21938@users.noreply.github.com>
2022-10-02 20:57:20 +0900

    Update main.js

--- assessment-finished/main.js
+++ assessment-finished/main.js
@@ -7,10 +7,10 @@ function randomValueFromArray(array){
   return array[random];
 }
 
-const storyText = '今日は華氏94度だったので' :insertx:' は釣りに出かけました。彼らは' :inserty:'に行くと、喧嘩を始め、':insertz:'のようになった. 私は魚を食べたかったのに食べられませんでした。 —' :insertx: 'はとてもメガネの似合う200ポンドのイケメンでした。';
-const insertX = ['サンジ', '太郎', 'ハリー'];
-const insertY = ['ディズニー', '酒場', '国会議事堂'];
-const insertZ = ['チーター', '炎', 'コック'];
+const storyText = "今日は華氏94度だったので:insertx:は釣りに出かけました。彼らは:inserty:に行くと、喧嘩を始め、:insertz:のように戦った。 私は魚を食べたかったのに食べられませんでした。 —:insertx: はとてもメガネの似合う200ポンドのイケメンでした。";
+const insertX = ["サンジ", "太郎", "ハリー"];
+const insertY = ["ディズニー", "酒場", "国会議事堂"];
+const insertZ = ["チーター", "炎", "コック"];
 
 randomize.addEventListener('click', result);
 
@@ -28,14 +28,14 @@ function result() {
 
   if (customName.value !== '') {
     const name = customName.value;
-    newStory = newStory.replace('二郎', name);
+    newStory = newStory.replace("二郎", name);
   }
 
   if (document.getElementById("日本").checked) {
     const weight = `${Math.round(200*0.0714286)} stone`;
     const temperature =  `${Math.round((94-32) * 5 / 9)} centigrade`;
-    newStory = newStory.replace('華氏94度', temperature);
-    newStory = newStory.replace('200ポンド', weight);
+    newStory = newStory.replace("華氏94度", temperature);
+    newStory = newStory.replace("200ポンド", weight);
   }
 
   story.textContent = newStory;
